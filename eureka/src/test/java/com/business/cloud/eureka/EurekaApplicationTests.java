47513756453927db908fb32e007eb386b34cc7cd
15122878813 <zq0904jgs@163.com>
2020-12-19 17:28:33 +0800

    springcloud for study

---
+++ eureka/src/test/java/com/business/cloud/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.cloud.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
